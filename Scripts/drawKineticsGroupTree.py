1392359932a27cdc17fad82f19ca4d71255c15e2
Max Liu <mjliu@mit.edu>
2017-11-20 17:29:12 -0500

    Update November 20, 2017

--- Scripts/drawKineticsGroupTree.py
+++ Scripts/drawKineticsGroupTree.py
@@ -2,50 +2,70 @@
 # encoding: utf-8
 
 """
-This script draws the entire group tree for the specified kinetics family,
+This script draws the group tree for the specified kinetics family,
 including diagrams for each group definition.
 
-Saves output as a (very large) pdf.
+Optionally, a second argument can be provided to indicate a head node
+to start at. If specified, the output will only include the specified
+node and its children.
+
+Saves output as a pdf.
 """
 
 import os
 import argparse
 import pydot
 import rmgpy
+from collections import OrderedDict
+
 from rmgpy.data.kinetics import KineticsDatabase
 from rmgpy.molecule import Group
 
-def convert(family):
 
-    databasePath = rmgpy.settings['database.directory']
-    familyPath = os.path.join(databasePath, 'kinetics', 'families')
+def convert(family, top_node=None):
+
+    database_path = rmgpy.settings['database.directory']
+    family_path = os.path.join(database_path, 'kinetics', 'families')
 
     kinetics = KineticsDatabase()
-    kinetics.loadFamilies(familyPath, families=[family])
+    kinetics.loadFamilies(family_path, families=[family])
+
+    if top_node is not None:
+        selected_entries = OrderedDict()
+        selected_entries[top_node] = kinetics.families[family].groups.entries[top_node]
+        queue = [selected_entries[top_node]]
+        for item in queue:
+            if item.children:
+                queue.extend(item.children)
+                for child in item.children:
+                    selected_entries[child.label] = child
+    else:
+        selected_entries = kinetics.families[family].groups.entries
 
     groups = []
     graph = pydot.Dot(graph_type='graph', dpi='52')
-    for label, entry in kinetics.families[family].groups.entries.iteritems():
+    for label, entry in selected_entries.iteritems():
         if entry.index == -1:
             continue
         elif isinstance(entry.item, Group):
-            imagePath = drawGroup(label, entry.item)
-            nodeLabel = '<<TABLE border="0">\n<TR><TD>{1}</TD></TR>\n<TR><TD><IMG scale="True" SRC="{0}"/></TD></TR>\n</TABLE>>'.format(os.path.join(imagePath), label)
+            image_path = draw_group(label, entry.item)
+            node_label = '<<TABLE border="0">\n<TR><TD>{1}</TD></TR>\n<TR><TD><IMG scale="True" SRC="{0}"/></TD></TR>\n</TABLE>>'.format(os.path.join(image_path), label)
         else:
-            nodeLabel = str(entry.item)
-            nodeLabel = '"' + label + '\n' + nodeLabel + '"'
+            node_label = str(entry.item)
+            node_label = '"' + label + '\n' + node_label + '"'
 
         groups.append(label)
         index1 = groups.index(label) + 1
-        graph.add_node(pydot.Node(name=str(index1), label=nodeLabel, fontname="Helvetica", fontsize="16", shape="box"))
-        if entry.parent is not None:
+        graph.add_node(pydot.Node(name=str(index1), label=node_label, fontname="Helvetica", fontsize="16", shape="box"))
+        if entry.label != top_node and entry.parent is not None:
             index2 = groups.index(entry.parent.label) + 1
             graph.add_edge(pydot.Edge(src=str(index2), dst=str(index1), fontname="Helvetica", fontsize="16"))
 
     outputPath = family + '_groups.pdf'
     graph.write_pdf(outputPath, prog='dot')
 
-def drawGroup(label, group):
+
+def draw_group(label, group):
 
     label = label.replace('/', '_')
     path = os.path.join(os.getcwd(), 'images', label + '.png')
@@ -65,9 +85,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('family', metavar='FAMILY', type=str, nargs=1,
-        help='The name of the kinetics family')
+                        help='The name of the kinetics family')
+    parser.add_argument('head', metavar='HEAD', type=str, nargs='?', default=None,
+                        help='The top node to start at')
 
     args = parser.parse_args()
     family = args.family[0]
+    head = args.head
 
-    convert(family)
+    convert(family, head)
